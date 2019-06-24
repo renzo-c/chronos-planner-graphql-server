89a0072bbb98d6085a8af9146bf6c22c0e93fffd
Renzo <rcnavarrop@gmail.com>
2019-06-24 16:38:38 -0500

    crud actions implemented in typedefs and resolvers of employee model

--- src/employee/typeDef.js
+++ src/employee/typeDef.js
@@ -18,7 +18,7 @@ export default `
 
     type Query {
         employees: [Employee!]!,
-        employee(id: ID!): Employee 
+        employee(id: ID!): Employee
     }
 
     type Mutation {
@@ -32,5 +32,17 @@ export default `
             phone: String!,
             email: String!,
         ): Employee!
+        updateEmployee(
+            id: ID!,
+            firstName: String,
+            lastName: String,
+            user: String,
+            password: String,
+            dni: Int,
+            address: String,
+            phone: String,
+            email: String,
+        ): Employee!
+        deleteEmployee( id: ID! ): Employee
     }
 `;
